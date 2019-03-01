c8a420ae179cd84d38c652c88e60a23173e98404
grantfogle <grantjfogle@gmail.com>
2019-02-28 17:20:38 -0700

    resubmitting changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Navigation from './components/Navigation';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <>
+        <Navigation />
         <Container>
           <Row>
             <Col>Stuff</Col>
