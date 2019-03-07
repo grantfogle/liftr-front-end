2aada575c292e82718d324421be839ebec8c2637
grantfogle <grantjfogle@gmail.com>
2019-03-06 22:26:57 -0700

    removing schedule component

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
           {/* <MiniNavigation /> */}
           {/* <DailyWorkout /> */}
           <Goals />
-          <Schedule />
+          {/* <Schedule /> */}
         </div>
         <Footer />
       </div>
