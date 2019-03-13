a7eaf43a9f2205376a5615a73acb59e6b2a5e658
grantfogle <grantjfogle@gmail.com>
2019-03-12 22:21:32 -0600

    adding router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import DailyWorkout from './components/DailyWorkout';
 import Footer from './components/Footer';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 /* create a navigation
 add interactivity
