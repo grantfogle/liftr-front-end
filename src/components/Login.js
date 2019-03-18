6131f3591376ee826a9116467088952deeb9ad49
grantfogle <grantjfogle@gmail.com>
2019-03-18 16:42:27 -0600

    updating login component with form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login Form</h1>
-
-            </div>
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                </Form.Row>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         );
     }
 }
