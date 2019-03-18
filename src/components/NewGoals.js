7c62122c60b9129aad0e7f79c41aefdb6ba9ceb0
grantfogle <grantjfogle@gmail.com>
2019-03-18 17:07:29 -0600

    showing up new goals component

--- src/components/NewGoals.js
+++ src/components/NewGoals.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+
 
 const NewGoals = () => {
-    return (
 
-        <h1>cats</h1>
+    //button for new row
+    //table input
+
+    return (
+        <div className="new-goals">
+            {/* <Container className="new-goals"> */}
+            <h1>Create new workout</h1>
+            {/* </Container> */}
+        </div>
     );
 }
 
