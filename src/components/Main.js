1463283614a9cb4a16b72d86b04051f746cdd71d
Grant <grant.fogle@davita.com>
2019-11-15 15:18:25 -0700

    notes

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import TopNav from './TopNav/TopNav';
 import SideNav from './SideNav/SideNav';
 import ProgressDashboard from './Dashboards/ProgressDashboard';
 import './Main.scss';
-
+// i think i need to add another router inside this component.
 class Main extends Component {
     state = {
     };
