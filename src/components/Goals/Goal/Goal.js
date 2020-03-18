1087d08a6e2a1e340500f724b44207052d6a46f7
grantfogle <grantjfogle@gmail.com>
2020-03-17 18:53:55 -0600

    updated goal component

--- src/components/Goals/Goal/Goal.js
+++ src/components/Goals/Goal/Goal.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Goal.scss';
 
+const getPercentage = (numerator, denominator) => {
+    let num = (numerator / denominator).toFixed(2) * 100;
+    return num + '%';
+}
+
 const Goal = ({ goal }) => {
-    let { name, category, timeline, current } = goal;
+    let { name, category, timeline, currentStat, goalStat } = goal;
     return (
-        <div class="goal">
-            <p>asdfasd</p>
-            <h2>{name} <span>{category}</span></h2>
-            <p>{timeline}</p>
-            <p>{current}</p>
-        </div >
+        <div className='goal'>
+            <div>
+                <h4>{name} <span>{category}</span></h4>
+                <p className='text-secondary'>Due Date: {timeline}</p>
+            </div>
+            <div className='goal__stats'>
+                <p className=''>{getPercentage(currentStat, goalStat)}</p>
+                <button className='goal__stats__button'>View Workout</button>
+                <div className='goal__stats__settings'></div>
+            </div>
+        </div>
     );
 }
 
--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -3,11 +3,11 @@ import Goal from './Goal/Goal';
 import './Goals.scss';
 
 let currentGoals = [
-    { name: 'Bench Press', category: 'Strength', currentStat: '185', goal: '420', timeline: 'Dec 21, 2020' },
-    { name: 'Squat', category: 'Strength', currentStat: '225', goal: '315', timeline: 'Dec 21, 2020' },
-    { name: 'Deadlift', category: 'Strength', currentStat: '305', goal: '405', timeline: 'Dec 21, 2020' },
-    { name: 'Half Marathon', category: 'Cardio', currentStat: '2:21', goal: '2:00', timeline: 'Dec 21, 2020' },
-    { name: 'Lose 10 Pounds', category: 'Weight Loss', currentStat: '185', goal: '175', timeline: 'Jan 21, 2021' },
+    { name: 'Bench Press', category: 'Strength', currentStat: '185', goalStat: '420', timeline: 'Dec 21, 2020' },
+    { name: 'Squat', category: 'Strength', currentStat: '225', goalStat: '315', timeline: 'Dec 21, 2020' },
+    { name: 'Deadlift', category: 'Strength', currentStat: '305', goalStat: '405', timeline: 'Dec 21, 2020' },
+    { name: 'Half Marathon', category: 'Cardio', currentStat: '2:21', goalStat: '2:00', timeline: 'Dec 21, 2020' },
+    { name: 'Lose 10 Pounds', category: 'Weight Loss', currentStat: '185', goalStat: '175', timeline: 'Jan 21, 2021' },
 ];
 
 let displayGoals = (goals) => goals.map((goal) => <Goal goal={goal} />);
