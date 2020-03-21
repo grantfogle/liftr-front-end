25662321a88f96c6a34e5b05728d315701190a6d
grantfogle <grantjfogle@gmail.com>
2020-03-21 15:13:05 -0600

    updated workout to stateful component

--- src/components/Goals/Goal/Goal.js
+++ src/components/Goals/Goal/Goal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Goal.scss';
 
@@ -7,26 +7,42 @@ const getPercentage = (numerator, denominator) => {
     return num + '%';
 }
 
-const Workout = ({ workout }) => {
-    let { name, category, timeline, currentStat, goalStat } = workout;
-    return (
-        <div className='workout'>
-            <div className="workout__headline">
-                <div className="workout__headline__title">
-                    <h4>{name} <span>{category}</span></h4>
-                    <p className='text-secondary'>Due Date: {timeline}</p>
-                </div>
-                <div className='workout__headline__stats'>
-                    <p className=''>{getPercentage(currentStat, goalStat)}</p>
-                    <button className='workout__headline__stats__button'>View Progress</button>
-                    <FontAwesomeIcon icon='ellipsis-v' />
-                </div>
-            </div>
-            <div className='workout__view'>
+class Workout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showWorkout: false,
+        }
+    }
+    toggleWorkoutView = () => {
+        this.setState({ showWorkout: !this.state.showWorkout });
+        console.log('cats');
+    };
+    render() {
+        const { name, category, timeline, currentStat, goalStat } = this.props.workout;
+        return (
+            <div className='workout' onClick={this.toggleWorkoutView}>
 
+                <div className="workout__headline">
+
+                    <div className="workout__headline__title">
+                        <h4>{name} <span>{category}</span></h4>
+                        <p className='text-secondary'>Due Date: {timeline}</p>
+                    </div>
+
+                    <div className='workout__headline__stats'>
+                        <p className=''>{getPercentage(currentStat, goalStat)}</p>
+                        <button className='workout__headline__stats__button'>View Progress</button>
+                        <FontAwesomeIcon icon='ellipsis-v' />
+                    </div>
+
+                </div>
+                <div className={this.state.showWorkout ? 'workout__show' : 'workout__hide'}>
+                    <h4>Workout bruh</h4>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Workout;
