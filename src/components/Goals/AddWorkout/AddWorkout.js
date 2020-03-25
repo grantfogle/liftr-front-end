412a017fe86f624337dde6fa87cf960ecad9bec9
Grant <grant.fogle@davita.com>
2020-03-25 16:18:34 -0600

    updating workout form

--- src/components/Goals/AddWorkout/AddWorkout.js
+++ src/components/Goals/AddWorkout/AddWorkout.js
@@ -26,11 +26,9 @@ class AddWorkout extends Component {
                 <h4>Create a New Workout</h4>
                 <form>
                     <div className="add-workout-form__section">
-                        <div className="mb-1">
-                            <h6>Workout Name</h6>
-                            <input placeholder="Enter Name" type="text"></input>
-                        </div>
-                        <h6>Workout Type</h6>
+                        <label>Name</label>
+                        <input placeholder="Gainz for God" type="text"></input>
+                        <label>Type</label>
                         <select id="type" name="type">
                             <option  value="bodyweight">Bodyweight</option>
                             <option  value="kettlebells">Kettlebells</option>
@@ -40,9 +38,8 @@ class AddWorkout extends Component {
                         </select>
                     </div>
                     <div className="add-workout-form__section">
-                        <h6>How will you track the status of this workout?</h6>
                         <input placeholder="Goal Name" type="text"></input>
-                        <input placeholder="Current Best" type="text"></input>
+                        <input placeholder="Current" type="text"></input>
                         <input placeholder="Goal" type="text"></input>
                         <select id="type" name="type">
                             <option  value="weight">Pounds</option>
@@ -53,15 +50,15 @@ class AddWorkout extends Component {
                     <div className="add-workout-form__section add-workout-form__workout">
                         <h5>Enter Workout Below:</h5>
                             <div className="add-workout-form__workout__row">
-                            <input className="add-workout-form__workout__row__name" placeholder="Exerise" type="text"></input>
+                            <input className="add-workout-form__workout__row__name" placeholder="Exercise" type="text"></input>
                             <input placeholder="Weight" type="text"></input>
+                            <input placeholder="Reps" type="text"></input>
+                            <input placeholder="Sets" type="text"></input>
                             <select id="type" name="type">
                                 <option  value="pounds">Pounds</option>
                                 <option  value="reps">Reps</option>
                                 <option  value="time">Minutes</option>
                             </select>
-                            <input placeholder="Reps" type="text"></input>
-                            <input placeholder="Sets" type="text"></input>
                             <button>Delete</button>
                         </div>
                         <button>Add row</button>
